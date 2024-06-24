4fe0c739c05400e8d1301ccdef47da55f23ae6a7
ffozilbek <fozilbekfayzullayev05@gmail.com>
2024-06-24 21:11:17 +0500

    add localstorage

--- js/script.js
+++ js/script.js
@@ -1,10 +1,17 @@
 const btn = document.querySelector(".change-mode");
 const btnReverse = document.querySelector(".change-mode-reverse");
 
+if(localStorage.getItem("mode")==="dark-mode") {
+    document.querySelector("body").classList.add("dark-mode");
+    btn.style.display="none"
+    btnReverse.style.display="block";
+}
+
 btn.addEventListener("click", ()=> {
     document.querySelector("body").classList.add("dark-mode");
     btn.style.display="none"
     btnReverse.style.display="block";
+    localStorage.setItem("mode","dark-mode")
 })
 
 btnReverse.addEventListener("click",()=> {
